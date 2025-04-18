69ff0edd84804e7fa5d18c633f4cec90def29383
hamsar4j <hamsarajs8@gmail.com>
2025-04-19 00:27:44 +0800

    chore: done w qn 1

--- student_files/q1.py
+++ student_files/q1.py
@@ -1,10 +1,43 @@
 import sys
 from pyspark.sql import SparkSession
-# you may add more import if you need to
+from pyspark.sql.functions import col, length, regexp_replace
 
+# you may add more import if you need to
+import os
 
 # don't change this line
 hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
+
+base_path = os.getcwd()
+input_path = f"{base_path}/data/TA_restaurants_curated_cleaned.csv"
+df = spark.read.option("header", True).csv(input_path)
+
+df.printSchema()
+print("Original row count:", df.count())
+
+# Filter rows with no reviews or ratings < 3.0
+# From df.printSchema():  Rating is casted to String -> Type casting to float must be done
+cleaned_df = df.filter(
+    (col("Reviews").isNotNull())
+    & (col("Reviews") != "")
+    & (length(regexp_replace(col("Reviews"), r"[\[\],\s]", "")) > 0)
+    & (col("Rating").cast("float") >= 3.0)
+)
+
+print("Row count after filtering:", cleaned_df.count())
+
+# Write into the target path
+output_path = f"{base_path}/output/question1/"
+# output_path = f"hdfs://{hdfs_nn}:9000/assignment2/output/question1/"
+cleaned_df.write.mode("overwrite").option("header", True).csv(output_path)
+
+# check ans
+single_csv_path = f"{output_path}/q1_output.csv"
+cleaned_df.coalesce(1).write.mode("overwrite").option("header", True).csv(
+    single_csv_path
+)
+
+spark.stop()
